8dfc2206e8de15ef1efce5d3c4663a17b81d75ba
Elmano Neto <elmanoneto@gmail.com>
2020-04-08 13:40:12 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//AIzaSyC2n0o8PRDusVeC7DfnC7SS8TIgdnPEBpI
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import GoogleMapReact from 'google-map-react'
+
+
+const App: React.FC = () => {
+    const heatMapData = {
+        positions: [
+            { lat: -23.5329, lng: -46.6395, weight: 5682},
+            { lat: -22.9129, lng: -43.2003, weight: 1688 },
+            { lat: -3.71664, lng: -38.5423, weight: 1051}
+        ],
+        options: {
+            radius: 100,
+            opacity: 0.7
+        }
+    }
+
+    const center = {
+        lat: -10.1689,
+        lng: -48.3317
+      }
+
+    const zoom = 5
+
+    const apiKey = 'AIzaSyC2n0o8PRDusVeC7DfnC7SS8TIgdnPEBpI'
+    return (
+        <div style={{ height: '100vh', width: '100%' }}>
+            <GoogleMapReact 
+                bootstrapURLKeys={{
+                    key: apiKey,
+                    libraries: 'visualization'
+                }}
+                defaultCenter={center}
+                defaultZoom={zoom}
+                heatmapLibrary={true}
+                heatmap={heatMapData}
+            />
+        </div>
+    )
 }
 
-export default App;
+export default App
